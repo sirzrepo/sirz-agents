3f1d4c17d950bf6ae5401e1c9ec6b1c651239817
mhatons <mhatons@gmail.com>
2025-09-06 07:57:29 +0100

    integrted sitemaps on every project

--- brandcom/vite.config.ts
+++ brandcom/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sitemap({
+    hostname: "https://www.brandcom.store",
+    dynamicRoutes: [
+     "/",
+    ],
+    changefreq: 'monthly',
+    priority: 0.8,
+    lastmod: new Date(),
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
